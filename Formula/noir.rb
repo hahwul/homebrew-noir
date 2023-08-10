4d6612ef3f139efc69ccc772950f5e00864ce9c9
HAHWUL <hahwul@gmail.com>
2023-08-11 01:50:23 +0900

    Release v0.2.3

--- Formula/noir.rb
+++ Formula/noir.rb
@@ -1,9 +1,9 @@
 class Noir < Formula
   desc "Noir is an attack surface detector form source code."
   homepage "https://github.com/hahwul/noir"
-  url "https://github.com/hahwul/noir/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "263361098df7dbd5aae74c303636f225c086313d37d1ac1f2cd14248f9b15c60"
-  version "0.2.2"
+  url "https://github.com/hahwul/noir/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "40779e61ce3982408a42846749bb8edd53e6015f7e2647d57425367f2dfcd731"
+  version "0.2.3"
   depends_on "crystal"
 
   def install
